be03d91d50f702e1d4b1aa9ec2876efe503e87b0
ProtosL <644074553@qq.com>
2023-01-17 21:46:56 +0800

    vue3 子应用接入微前端

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -2,6 +2,26 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+let instance = null;
+const render = () => {
+    instance = createApp(App)
+    instance.use(router)
+        .mount('#app')
+}
+
+if (!window.__MICRO_WEB__) {
+    render();
+}
+
+export const bootstrap = () => {
+    console.log('开始加载')
+}
+
+export const mount = () => {
+    render();
+    console.log('渲染成功')
+}
+
+export const unmount = () => {
+    console.log('卸载', instance)
+}
