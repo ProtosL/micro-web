8fdf332e15c028fd8e2b5c2346ecbd9c52c934e0
ProtosL <644074553@qq.com>
2023-01-17 21:42:16 +0800

    vue2 子应用接入微前端

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -4,7 +4,27 @@ import router from './router';
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app-vue')
+let instance = null;
+const render = () => {
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app-vue')
+}
+
+if (!window.__MICRO_WEB__) {
+  render();
+}
+
+export const bootstrap = () => {
+  console.log('开始加载')
+}
+
+export const mount = () => {
+  render();
+  console.log('渲染成功')
+}
+
+export const unmount = () => {
+  console.log('卸载', instance)
+}
