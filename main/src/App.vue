764fc66f5371b3f90bfdcb7fca98b09c6a70ab02
ProtosL <644074553@qq.com>
2023-01-18 15:37:55 +0800

    主应用开发

---
+++ main/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <Header />
+    <MainNav />
+    <div class="sub-container">
+        <Loading v-show="loading" />
+        <div v-show="!loading" id="micro-container">子应用内容</div>
+    </div>
+    <Footer />
+</template>
+<script>
+import Header from "./components/Header";
+import MainNav from "./components/MainNav";
+import Loading from "./components/Loading";
+import Footer from './components/Footer';
+
+import { ref } from 'vue';
+
+export default {
+    name: 'App',
+    components: {
+        Header,
+        MainNav,
+        Loading,
+        Footer
+    },
+    setup() {
+        const loading = ref(true)
+        setTimeout(() => {
+            loading.value = false;
+        }, 2000)
+        return {
+            loading
+        }
+    }
+}
+</script>
+
+
+<style lang="scss">
+html,
+body,
+#micro_web_main_app {
+    width: 100%;
+    height: 100%;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.sub-container {
+    min-height: 100%;
+    position: relative;
+}
+
+#micro-container {
+    min-height: 100%;
+    width: 100%;
+}
+</style>
