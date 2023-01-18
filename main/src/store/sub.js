8f0b46da60a4fc9a2d3e18c2e835e42e21006f70
ProtosL <644074553@qq.com>
2023-01-18 16:39:34 +0800

    子应用注册

---
+++ main/src/store/sub.js
@@ -0,0 +1,38 @@
+// import { loading } from '../store'
+// import * as appInfo from '../store'
+
+export const subNavList = [
+    {
+        name: 'react15',// 子应用唯一 id
+        entry: '//localhost:9002/', // 入口
+        // loading,
+        container: '#micro-container', // 容器 div 的 id
+        activeRule: '/react15', // 子应用激活的路由规则
+        // appInfo,
+    },
+    {
+        name: 'react16',
+        entry: '//localhost:9003/',
+        // loading,
+        container: '#micro-container',
+        activeRule: '/react16',
+        // appInfo,
+    },
+    {
+        name: 'vue2',
+        entry: '//localhost:9004/',
+        // loading,
+        container: '#micro-container',
+        activeRule: '/vue2',
+        // appInfo,
+    },
+    {
+        name: 'vue3',
+        entry: '//localhost:9005/',
+        // loading,
+        container: '#micro-container',
+        activeRule: '/vue3',
+        // appInfo,
+    },
+];
+
