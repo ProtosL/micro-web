84fa1368f44a095d8dcfd2e82e69f7ac64711814
ProtosL <644074553@qq.com>
2023-01-19 15:40:59 +0800

    加载和解析 js

--- main/micro/loader/index.js
+++ main/micro/loader/index.js
@@ -9,7 +9,8 @@ export const loadHtml = async (app) => {
     // 子应用的入口
     let entry = app.entry;
 
-    const html = await parseHtml(entry);
+    const [ dom, scripts ] = await parseHtml(entry);
+    console.log(scripts)
 
     const ct = document.querySelector(container);
 
@@ -17,7 +18,7 @@ export const loadHtml = async (app) => {
         throw new Error('容器不存在');
     }
     
-    ct.innerHTML = html;
+    ct.innerHTML = dom;
 
     return app;
 }
@@ -25,15 +26,65 @@ export const loadHtml = async (app) => {
 export const parseHtml = async (entry) => {
     const html = await fetchResource(entry);
 
+    let allScript = [];
     const div = document.createElement('div');
     div.innerHTML = html;
-    // 标签 link script（src、js）
-    const [dom, scriptUrl, script] = await getResources();
-    console.log(dom, scriptUrl, script)
     
-    return html;
+    const [dom, scriptUrl, script] = await getResources(div, entry);
+    const fetchedScripts = await Promise.all(scriptUrl.map(async item => fetchResource(item)))
+
+    allScript = script.concat(fetchedScripts);
+    return [dom, allScript];
 }
 
-export const getResources = async () => {
-    return ['', '', ''];
+export const getResources = async (root, entry) => {
+    const scriptUrl = [];
+    const script = [];
+    const dom = root.outerHTML; // 当前根元素连同其中的所有内容
+    
+    // 深度解析
+    function deepParse(element) {
+        const children = element.children;
+        const parent = element.parent;
+
+        // 第一步处理位于 script 中的内容
+        if (element.nodeName.toLowerCase() === 'script') {
+            const src = element.getAttribute('src');
+            // 直接在 script 标签中写的内容
+            if (!src) {
+                script.push(element.outerHTML);
+            } else {
+                if (src.startsWith('http')) {
+                    scriptUrl.push(src);
+                } else {
+                    scriptUrl.push(`${window.location.protocol}${entry}/${src}`);
+                }
+            }
+
+            if (parent) {
+                parent.replaceChild(document.createComment('此 js 文件已被微前端替换'), element)
+            }
+        }
+
+        // link 也会有 js 的内容
+        if (element.nodeName.toLowerCase() === 'link') {
+            const href = element.getAttribute('href');
+
+            if (href.endsWith('.js')) {
+                if (href.startsWith('http')) {
+                    scriptUrl.push(href);
+                } else {
+                    scriptUrl.push(`${window.location.protocol}${entry}/${href}`);
+                }
+            }
+        }
+
+        for (let i = 0; i < children.length; i++) {
+            deepParse(children[i]);
+        }
+    }
+
+    deepParse(root);
+    
+    return [dom, scriptUrl, script];
 }
